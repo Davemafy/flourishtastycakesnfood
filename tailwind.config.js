baec0cb65fbb6a8829fa69396ae9b7e07e6ba728
Davemafy <imafidondavid1@gmail.com>
2023-12-13 12:05:49 +0100

    feat: implement rhe new design, added reponsive features

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,29 +1,21 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-	content: ['./*.html'],
-	theme: {
-		screens: {
-			sm: '480px',
-			md: '768px',
-			lg: '978px',
-			xl: '1440px'
-		},
-		fontFamily: {
-			cookie: ['Cookie',
-				'sans-serif'],
-			sans: ['Product',
-				'sans-serif'],
-		},
-		extend: {
-			colors: {
-				'white': '#fff',
-				'lightBrown': '#EBC6B0',
-				'darkBrown': '#EBC6B0',
-				'dark': '#111111',
-				'veryYellow': '#F8CB10',
-				'lighterDark': '#242424',
-			},
-		}
-	},
-	plugins: [],
-}
+    content: ["./*.html"],
+    theme: {
+        extend: {
+            colors: {
+                white: "#fff",
+                lightBrown: "#EBC6B0",
+                darkBrown: "#EBC6B0",
+                dark: "#111111",
+                veryYellow: "#F8CB10",
+                lighterDark: "#242424"
+            },
+            fontFamily: {
+                bd: ["romanBold", "sans-serif"],
+                roman: ["roman", "sans-serif"]
+            }
+        }
+    },
+    plugins: []
+};
